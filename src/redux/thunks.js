02d6632874a79c82743c5990b874a573a42a2f18
Ihor Poplawskyii <ihorpoplawskyii@Ihors-MacBook-Air.local>
2025-04-27 23:00:33 +0300

    test fetch

--- src/redux/thunks.js
+++ src/redux/thunks.js
@@ -6,9 +6,11 @@ export const signUpThunk = async ({username, email, password}) => {
   }  
   try {
     const response = await fetch(
-      `https://treasure-bay.onrender.com/users/sign-up`, {
-        method: 'POST',
-        body: obj,
+      `https://treasure-bay.onrender.com/users/sign-up`,
+      {
+        method: "POST",
+        body: JSON.stringify(obj),
+        headers: { "Content-type": "application/json" },
       }
     );
     const data = await response.json();
