f1f3c0caff1b86d1bc371f34ece37df87abbf2e8
Ihor Poplawskyii <poplawskyiihor@gmail.com>
2025-05-12 10:22:51 +0300

    fix vercel 1

--- src/pages/SignPage/SignPage.jsx
+++ src/pages/SignPage/SignPage.jsx
@@ -16,6 +16,7 @@ export const SignPage = () => {
   const result = useAppSelector((state) => state.user.result);
   const status = useAppSelector((state) => state.user.status);
   const logged = useAppSelector((state) => state.user.user.token);
+  console.log(form)
   
   useEffect(() => {
     dispatch(clearError());
